97781127a751b29831b1417af2820b86cee98b2e
yassine <bakiri.yassine@gmail.com>
2018-06-24 14:35:28 +0200

    add : call player.js and add features to it

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 import Point from './modules/Point.js';
+import Player from './modules/Player.js';
 
 var cursors;
 
@@ -18,29 +19,38 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+var player = new Player({
+    speed: 0,
+    direction: '',
+    bottomPos: 0,
+    sprite: null,
+    position: null,
+    isClimbing: false
+});
 
 function preload() {
     //this.load.image('sky', 'assets/sky.png');
+    player.preload(this);
 }
 
 function create() {
-
     cursors = this.input.keyboard.createCursorKeys();
-
     //this.add.image(400, 300, 'sky');
-
-
 }
 
 function update() {
 
     if (cursors.left.isDown) {
         //move left
+        player.player.setVelocityX(-160);
     } else if (cursors.right.isDown) {
         //move right
+        player.player.setVelocityX(160);
     } else if (cursors.up.isDown) {
         //move up
     } else if (cursors.down.isDown) {
         //move down
+    } else {
+        player.player.setVelocityX(0);
     }
 }
--- src/js/modules/Player.js
+++ src/js/modules/Player.js
@@ -5,14 +5,60 @@
  * -----------------------
  * @param  {Object} position
  * -----------------------
-**/
+ **/
 
 'use strict';
 import Point from './Point.js';
 
-export default class Player  {
-    constructor(constructorObject) {
+export default class Player {
 
+    constructor(constructorObject) {
+        this.speed =  constructorObject.speed;
+        this.direction =  constructorObject.direction;
+        this.bottomPos =  constructorObject.bottomPos;
+        this.sprite =  constructorObject.sprite;
+        this.position =  constructorObject.position;
+        this.isClimbing =  constructorObject.isClimbing;
+
+        this.player = null;
+
+        this.addSprites = this.addSprites.bind(this);
+        this.createAnims = this.createAnims.bind(this);
+    }
+
+    preload(phaserScene) {
+        phaserScene.load.spritesheet('dude', 'dist/assets/dude.png', {
+            frameWidth: 32,
+            frameHeight: 48
+        });
+        this.player = phaserScene.physics.add.sprite(100, 450, 'dude');
+        this.addSprites(phaserScene);
+    }
+
+    addSprites(phaserScene) {
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+        this.createAnims(phaserScene);
+    }
+
+    createAnims(phaserScene) {
+        phaserScene.anims.create({
+            key: 'left',
+            frames: phaserScene.anims.generateFrameNumbers('dude', {start: 0, end : 3}),
+            frameRate: 10,
+            repeat: -1
+        });
+        phaserScene.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+        phaserScene.anims.create({
+            key: 'right',
+            frames: phaserScene.anims.generateFrameNumbers('dude', {start: 5, end : 8}),
+            frameRate: 10,
+            repeat: -1
+        });
     }
 
 }
