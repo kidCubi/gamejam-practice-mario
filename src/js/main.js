0f84492c761cbe79de81d2300f6ed712bbcb0af1
yassine <bakiri.yassine@gmail.com>
2018-06-24 14:35:55 +0200

    Add : global physics

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,12 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+      default: 'arcade',
+        arcade: {
+          gravity: { y: 300 }
+        }
+    },
     scene: {
         preload: preload,
         create: create,
